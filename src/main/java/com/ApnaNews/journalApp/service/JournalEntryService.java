9d311b3d116a9bc0e6e0a38ae0675aee062642aa
Ayush Sharma <ayushsharma001.nsk@gmail.com>
2025-01-13 17:20:00 +0530

    Journal Entry Services commit

--- src/main/java/com/ApnaNews/journalApp/service/JournalEntryService.java
+++ src/main/java/com/ApnaNews/journalApp/service/JournalEntryService.java
@@ -2,20 +2,36 @@ package com.ApnaNews.journalApp.service;
 
 import com.ApnaNews.journalApp.entity.JournalEntry;
 import com.ApnaNews.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JournalEntryService {
 
     @Autowired
-    private JournalEntryRepository journalEntryRepository;
+    private  JournalEntryRepository journalEntryRepository;
 
 
     public void saveEntry(JournalEntry JournalEntry){
         journalEntryRepository.save(JournalEntry);
+    }
+
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+
+    public  JournalEntry findById(ObjectId id){
+
+        return journalEntryRepository.findById(id.toString());
+    }
+
+    public void deleteById(Integer id){
+        journalEntryRepository.deleteById(id);
     }
 }
 
