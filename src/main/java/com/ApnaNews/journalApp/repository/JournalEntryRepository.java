04ce9f7cfea7ae63423a886fb0bb6fe5f08d4015
Ayush Sharma <ayushsharma001.nsk@gmail.com>
2025-01-13 17:15:57 +0530

    Journal Entry Repository commit

--- src/main/java/com/ApnaNews/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/com/ApnaNews/journalApp/repository/JournalEntryRepository.java
@@ -1,7 +1,12 @@
 package com.ApnaNews.journalApp.repository;
 
 import com.ApnaNews.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface JournalEntryRepository extends MongoRepository <JournalEntry, String> {
+@Repository
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, Integer> {
+
+    public JournalEntry findById(String objectId);
 }
