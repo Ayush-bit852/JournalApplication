cf0672a4ba1090fad4101ccee4c9c2b129d1d1c4
Ayush Sharma <ayushsharma001.nsk@gmail.com>
2025-01-14 18:10:33 +0530

    Repository

--- src/main/java/com/ApnaNews/journalApp/repository/JournalEntryRepository.java
+++ src/main/java/com/ApnaNews/journalApp/repository/JournalEntryRepository.java
@@ -5,8 +5,13 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface JournalEntryRepository extends MongoRepository<JournalEntry, Integer> {
 
-    public JournalEntry findById(String objectId);
+
+    Optional<JournalEntry> findById(String objectId);
+
+
 }
